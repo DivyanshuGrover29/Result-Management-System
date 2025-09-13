2c442d398baf8dc2cd6d7ed5e384108fc0c90bb0
Divyanshu Grover <142349410+DivyanshuGrover29@users.noreply.github.com>
2025-09-13 21:26:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://result-management-system-34oc.vercel.app/',
+    method:['GET','POST','PUT','DELETE'],
     credentials:true
 };
 
