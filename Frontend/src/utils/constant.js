b057500931375325d52a83530702d82ab9183acf
Divyanshu Grover <142349410+DivyanshuGrover29@users.noreply.github.com>
2025-09-13 21:50:20 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const ADMIN_API_END_POINT="https://result-management-system-0429.onrender.com/api/v1/admin";
-export const RESULTS_API_END_POINT="https://result-management-system-0429.onrender.com/api/v1/result";
-export const DASHBOARD_API_END_POINT="https://result-management-system-0429.onrender.com/api/v1/dashboard";
-export const UPLOAD_API_END_POINT="https://result-management-system-0429.onrender.com/api/v1/excelupload";
+export const ADMIN_API_END_POINT="https://result-management-system-5p2l.onrender.com/api/v1/admin";
+export const RESULTS_API_END_POINT="https://result-management-system-5p2l.onrender.com/api/v1/result";
+export const DASHBOARD_API_END_POINT="https://result-management-system-5p2l.onrender.com/api/v1/dashboard";
+export const UPLOAD_API_END_POINT="https://result-management-system-5p2l.onrender.com/api/v1/excelupload";
